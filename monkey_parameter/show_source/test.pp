cadd66365fcb844f4202a4ddb5ae5d5115bc3b99
Richard Clamp <richardc@unixbeard.net>
2013-07-12 10:48:21 +0100

    the henchman was overkill

--- monkey_parameter/show_source/test.pp
+++ monkey_parameter/show_source/test.pp
@@ -1,7 +1,3 @@
-henchman { 'Puppet::Type::File::Show_source':
-  type     => 'file',
-  property => 'show_source',
-} ->
 file { '/tmp/show_source':
   ensure      => 'present',
   content     => 'test content',
